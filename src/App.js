ce38b5b63e8d4b78826ed5fac4b4ed44e39ebc56
Vito Monaco <vi.monaco@reply.it>
2018-07-13 14:19:40 +0200

    Rimossi file inutili

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Graph from './Graph';
-// import logo from './logo.svg';
 import './css/App.css';
 
 class App extends Component {
@@ -9,11 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Esercizio reti store and forward</h1>
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
         </header>
-        {/* <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
         <Graph className="fullPage"/>
       </div>
     );
